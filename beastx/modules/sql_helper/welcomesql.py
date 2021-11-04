447e68cfb8cbd7331875e2d86065beeae2ad1427
Mrunal <80758264+msy1717@users.noreply.github.com>
2021-11-04 10:17:55 +0530

    Update welcomesql.py

--- beastx/modules/sql_helper/welcomesql.py
+++ beastx/modules/sql_helper/welcomesql.py
@@ -1,6 +1,6 @@
 from sqlalchemy import BigInteger, Column, Numeric, String, UnicodeText
 
-from firebot.modules.sql_helper import BASE, SESSION
+from beastx.modules.sql_helper import BASE, SESSION
 
 
 class JoinWelcome(BASE):
