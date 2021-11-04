823ca95189a82a3c96dee97afeab7c330793e62b
Mrunal <80758264+msy1717@users.noreply.github.com>
2021-11-04 10:06:54 +0530

    Update PostAppender_sql.py

--- beastx/modules/sql_helper/PostAppender_sql.py
+++ beastx/modules/sql_helper/PostAppender_sql.py
@@ -1,6 +1,6 @@
 from sqlalchemy import Boolean, Column, String, UnicodeText
 
-from firebot.modules.sql_helper import BASE, SESSION
+from beastx.modules.sql_helper import BASE, SESSION
 
 
 class Pa(BASE):
