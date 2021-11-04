10a3abec37ce03c8ac71f07d088120b41276be23
Mrunal <80758264+msy1717@users.noreply.github.com>
2021-11-04 12:06:17 +0530

    Create helper.py

---
+++ beastx/modules/helper.py
@@ -0,0 +1,45 @@
+from beastx import ALIVE_NAME, CMD_LIST, lang
+from beastx.utils import beastx_cmd
+from beastx import beast
+
+
+DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "Beast-X"
+
+
+@beast.on(beastx_cmd(pattern="help ?(.*)"))
+async def cmd_list(event):
+    if not event.text[0].isalpha() and event.text[0] not in ("/", "#", "@", "!"):
+        tgbotusername = Var.TG_BOT_USER_NAME_BF_HER
+        input_str = event.pattern_match.group(1)
+        if tgbotusername is None or input_str == "text":
+            string = ""
+            for i in CMD_LIST:
+                string += "ℹ️ " + i + "\n"
+                for iter_list in CMD_LIST[i]:
+                    string += "    `" + str(iter_list) + "`"
+                    string += "\n"
+                string += "\n"
+            if len(string) > 4095:
+                await borg.send_message(event.chat_id, "Do .help cmd")
+                await asyncio.sleep(5)
+            else:
+                await event.edit(string)
+        elif input_str:
+            if input_str in CMD_LIST:
+                string = "Commands found in {}:\n".format(input_str)
+                for i in CMD_LIST[input_str]:
+                    string += "    " + i
+                    string += "\n"
+                await event.edit(string)
+            else:
+                await event.edit(input_str + " is not a valid plugin!")
+        else:
+            help_string = f"""Beast-X plugins Are Listed Here !\n
+For More Help or Support contact {DEFAULTUSER}\nCurrently Used Language - {lang}"""
+            results = await bot.inline_query(  # pylint:disable=E0602
+                tgbotusername, help_string
+            )
+            await results[0].click(
+                event.chat_id, reply_to=event.reply_to_msg_id, hide_via=True
+            )
+            await event.delete()
