e132efd7f49d1d81209f7af81d12b2e7963e0138
Mrunal <80758264+msy1717@users.noreply.github.com>
2021-11-04 09:05:38 +0530

    Update and rename firebotmain.py to BeastX.py

---
+++ BeastX.py
@@ -0,0 +1,5 @@
+import os
+
+os.system(
+    "git clone https://github.com/msy1717/BeastX /root/firebot && mkdir /root/beastx/bin/ && cd /root/beastx/ && chmod +x /usr/local/bin/* && python3 -m beastx"
+)
--- firebotmain.py
+++
@@ -1,5 +0,0 @@
-import os
-
-os.system(
-    "git clone https://github.com/TeamEviral/Fire-X /root/firebot && mkdir /root/firebot/bin/ && cd /root/firebot/ && chmod +x /usr/local/bin/* && python3 -m firebot"
-)
