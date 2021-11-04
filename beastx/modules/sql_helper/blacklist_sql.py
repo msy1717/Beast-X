af667b46a9aad1233eabcb8a42fd63b64f4bbe43
Mrunal <80758264+msy1717@users.noreply.github.com>
2021-11-04 10:07:44 +0530

    Update blacklist_sql.py

--- beastx/modules/sql_helper/blacklist_sql.py
+++ beastx/modules/sql_helper/blacklist_sql.py
@@ -2,7 +2,7 @@ import threading
 
 from sqlalchemy import Column, String, UnicodeText, distinct, func
 
-from firebot.modules.sql_helper import BASE, SESSION
+from beastx.modules.sql_helper import BASE, SESSION
 
 
 class BlackListFilters(BASE):
