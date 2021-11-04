ce68cdbc0fa6c1dbbc45a8d7131947e5930ddd0a
Mrunal <80758264+msy1717@users.noreply.github.com>
2021-11-04 09:08:30 +0530

    Update BeastX.py

--- BeastX.py
+++ BeastX.py
@@ -1,5 +1,5 @@
 import os
 
 os.system(
-    "git clone https://github.com/msy1717/BeastX /root/firebot && mkdir /root/beastx/bin/ && cd /root/beastx/ && chmod +x /usr/local/bin/* && python3 -m beastx"
+    "git clone https://github.com/msy1717/Beast-X /root/firebot && mkdir /root/beastx/bin/ && cd /root/beastx/ && chmod +x /usr/local/bin/* && python3 -m beastx"
 )
