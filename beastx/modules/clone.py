267d46f03edae6fde8c59e2021c9cd2113292602
Mrunal <80758264+msy1717@users.noreply.github.com>
2021-11-05 19:25:35 +0530

    Update clone.py

--- beastx/modules/clone.py
+++ beastx/modules/clone.py
@@ -18,6 +18,7 @@ from telethon.tl.functions.photos import (DeletePhotosRequest,
 from telethon.tl.types import InputPhoto, MessageMediaPhoto, User, Chat, Channel
 from beastx import beast, CMD_HELP , AUTONAME , DEFAULT_BIO , ALIVE_NAME
 from . import *
+#sed
 BEASTX = " "
 DEFAULTUSER = OWNER_NAME
 DEFAULTUSERBIO = str(DEFAULT_BIO) if DEFAULT_BIO else "Beast is op"
