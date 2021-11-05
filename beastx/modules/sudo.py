2cdfe1dc29b5c097f564d1344bdcd78fdd1e6625
Mrunal <80758264+msy1717@users.noreply.github.com>
2021-11-05 11:28:30 +0530

    Create sudo.py

---
+++ beastx/modules/sudo.py
@@ -0,0 +1,86 @@
+#@Godmrunal
+# COPY WITH CREDITS 
+import heroku3
+import re, os
+from var import Var
+from ..utils import admin_cmd
+from beastx import beast
+MRUNAL = Var.HEROKU_APP_NAME
+SEMX = Var.HEROKU_API_KEY
+sudolist = os.environ.get("SUDO_USERS", None)
+@beast.on(admin_cmd(pattern='addsudo'))
+async def add_sudo(event):
+  Heroku = heroku3.from_key(SEMX)
+  app = Heroku.app(MRUNAL)
+  heroku_var = app.config()
+  if not event.is_reply:
+    return await event.edit("ʀᴇᴘʟʏ ᴛᴏ ᴀ ᴍᴇssᴀɢᴇ ᴘʟᴇᴀsᴇ")                              
+  if event.is_reply:
+    id = (await event.get_reply_message()).sender_id
+    name = (await bot.get_entity(id)).first_name
+    sudo = heroku_var["SUDO_USERS"]
+    op = re.search(str(id), str(sudolist))
+    if op:
+      await event.edit(f"THE {name} IS ALREADY ON SUDO LIST")
+      return
+    else:
+      pass
+    if not sudolist:
+       await event.edit(f"Oᴋᴀʏ **{name}** ɪs Aᴅᴅᴇᴅ Oɴ sᴜᴅᴏ ʟɪsᴛ (ᴘʟᴇᴀsᴇ ᴡᴀɪᴛ ɪ ᴀᴍ ʀᴇsᴛᴀʀᴛɪɴɢ)")
+       heroku_var["SUDO_USERS"] = id
+    else:
+       sudousers = f'{sudolist} {id}'
+       await event.edit(f"Oᴋᴀʏ **{name}** ɪs ᴀᴅᴅᴇᴅ ᴏɴ sᴜᴅᴏ ᴜsᴇʀs (ᴘʟᴇᴀsᴇ ᴡᴀɪᴛ ɪ ᴀᴍ ʀᴇsᴛᴀʀᴛɪɴɢ)")
+       heroku_var["SUDO_USERS"] = sudousers
+  else:
+    await event.edit("ʀᴇᴘʟʏ ᴛᴏ ᴀ ᴍᴇssᴀɢᴇ ᴘʟᴇᴀsᴇ")                              
+
+
+
+@beast.on(admin_cmd(pattern='rmsudo'))
+async def remove_sudo(event):
+  Heroku = heroku3.from_key(SEMX)
+  app = Heroku.app(MRUNAL)
+  heroku_var = app.config()
+  if not event.is_reply:
+    return await event.edit("ʀᴇᴘʟʏ ᴛᴏ ᴀ ᴍᴇssᴀɢᴇ ᴘʟᴇᴀsᴇ")
+  if event.is_reply:
+    id = (await event.get_reply_message()).sender_id
+    name = (await bot.get_entity(id)).first_name
+    op = re.search(str(id), str(sudolist))
+    if op:
+      i = ""
+      amazing = sudolist.split(" ")
+      amazing.remove(str(id))
+      i += str(amazing)
+      x = i.replace("[", "")
+      xx = x.replace("]", "")
+      xxx = xx.replace(",", "")
+      done = xxx.replace("'", "")
+      heroku_var["SUDO_USERS"] = done
+      await event.edit(f"Tʜᴇ **{name}** ɪs ʀᴇᴍᴏᴠᴇᴅ sᴜᴄᴄᴇssғᴜʟʟʏ (ᴘʟᴇᴀsᴇ ᴡᴀɪᴛ ɪ ᴀᴍ ʀᴇsᴛᴀʀᴛɪɴɢ)")
+    else:
+      await event.edit(f"ᴛʜᴇ {name} ɪs ɴᴏᴛ ɪɴ sᴜᴅᴏ 😑😑")
+    if heroku_var["SUDO_USERS"] == None:
+       await event.edit(f"ᴛʜᴇ sᴜᴅᴏ ʟɪsᴛ ɪs ᴇᴍᴘʏᴛʏ 😑😑")
+@beast.on(admin_cmd("sudo"))
+async def sudos(event):
+  if sudolist:
+    await event.edit("sᴜᴅᴏ ɪs ᴇɴᴇᴀʙʟᴇᴅ ᴛʏᴘᴇ `.listsudo` ғᴏʀ sᴜᴅᴏ ᴜsᴇʀs ʟɪsᴛ")
+  else:
+     await event.edit("sᴜᴅᴏ ɪs ᴏғғ")            
+@beast.on(admin_cmd("listsudo"))
+async def sudolists(event):
+  op = await event.edit('ᴄʜᴇᴄᴋɪɴɢ ᴀʟʟ sᴜᴅᴏs ᴡᴀɪᴛ')
+  Heroku = heroku3.from_key(SEMX)
+  app = Heroku.app(MRUNAL)
+  heroku_var = app.config()
+  if not sudolist:
+    return await event.edit("sᴜᴅᴏ ʟɪsᴛ ɪs ᴇᴍᴘᴛʏ")
+  sudos = sudolist.split(" ")
+  sudoz = "**»sᴜᴅᴏ ʟɪsᴛ«**"
+  for sudo in sudos:
+    k = await bot.get_entity(int(sudo))
+    pro = f'\n[**ɴᴀᴍᴇ:** {k.first_name} \n**ᴜsᴇʀɴᴀᴍᴇ:** @{k.username or None}]\n'
+    sudoz += pro
+  await op.edit(sudoz)
