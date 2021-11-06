df4df51f8ad16f1c40f70926e89f1ce23366c7fd
Mrunal <80758264+msy1717@users.noreply.github.com>
2021-11-06 20:35:58 +0530

    Update __main__.py

--- beastx/__main__.py
+++ beastx/__main__.py
@@ -40,7 +40,7 @@ else:
 import glob
 async def ready():
     MSG = f"**Beast has been deployed!**\n➖➖➖➖➖➖➖➖➖\n**UserMode**: [{bot.me.first_name}](tg://user?id={bot.me.id})\n**Assistant**: @\n➖➖➖➖➖➖➖➖➖\n**Support**: @BeastX_Bots\n"
-    await beast.send_message(chat_id, MSG)
+    await bot.send_message(chat_id, MSG)
 
         
 path = "beastx/modules/*.py"
