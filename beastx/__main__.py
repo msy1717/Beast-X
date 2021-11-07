3fc05027696a5960ab1812a2fbcbc3ee07869230
Mrunal <80758264+msy1717@users.noreply.github.com>
2021-11-07 10:58:25 +0530

    Update __main__.py

--- beastx/__main__.py
+++ beastx/__main__.py
@@ -75,10 +75,12 @@ async def legend():
 
 if config.GET_MSG == "ENABLE":
     try:
-        PROBOYX = [[Button.inline("Hᴇʀᴏᴋᴜ Vᴀʀs", data='ass_back')]]
-        await xmrunal.send_message(chat_id, LEGENDX, buttons=PROBOYX)
+      PROBOYX = [[Button.inline("Hᴇʀᴏᴋᴜ Vᴀʀs", data='ass_back')]]
+        
+
+      await xmrunal.send_message(chat_id, LEGENDX, buttons=PROBOYX)
     except:
-        pass
+       pass
         
 if Config.ENABLE_ASSISTANTBOT == "ENABLE":
     path = "beastx/modules/assistant/*.py"
