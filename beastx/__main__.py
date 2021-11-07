da97f18ee6f32ac7a3fa8962caf8ebdf22aee283
Mrunal <80758264+msy1717@users.noreply.github.com>
2021-11-07 19:04:06 +0530

    Update __main__.py

--- beastx/__main__.py
+++ beastx/__main__.py
@@ -12,6 +12,8 @@ from beastx.Configs import Config
 from telethon.tl.functions.messages import AddChatUserRequest
 from beastx.utils import load_module, start_assistant
 import asyncio
+from . import sedmrunal
+
 sur = Config.PRIVATE_GROUP_ID
 
 UL = Config.TG_BOT_USER_NAME_BF_HER
@@ -25,7 +27,7 @@ async def add_bot(bot_token):
     await bot.start(bot_token)
     bot.me = await bot.get_me()
     bot.uid = telethon.utils.get_peer_id(bot.me)
-    await bot.send_message(sur, MSG)
+    await sedmrunal.send_message(sur, MSG)
     
 
 if len(argv) not in (1, 3, 4):
