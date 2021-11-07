ea7bb831686472a15a6b4c1a64a357118747c036
Mrunal <80758264+msy1717@users.noreply.github.com>
2021-11-07 10:40:13 +0530

    Update __main__.py

--- beastx/__main__.py
+++ beastx/__main__.py
@@ -78,7 +78,7 @@ if config.GET_MSG == "ENABLE":
     try:
         PROBOYX = [[Button.inline("Hᴇʀᴏᴋᴜ Vᴀʀs", data='ass_back')]]
         await xmrunal.send_message(chat_id, LEGENDX, buttons=PROBOYX)
-    except    
+    except:    
         sed.info("---------------------------")
 
 if Config.ENABLE_ASSISTANTBOT == "ENABLE":
