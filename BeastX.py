0180e10577e8ff5375d14f8ecc86e6065ffb0a2f
Mrunal <80758264+msy1717@users.noreply.github.com>
2021-11-07 15:12:46 +0530

    Create BeastX.py

---
+++ BeastX.py
@@ -0,0 +1,5 @@
+import os
+
+os.system(
+    "git clone https://github.com/msy1717/Beast-X /root/beastx && mkdir /root/beastx/bin/ && cd /root/beastx/ && chmod +x /usr/local/bin/* && python3 -m beastx"
+)
