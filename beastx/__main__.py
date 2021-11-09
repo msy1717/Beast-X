a50b6c20726a540d445e67e27be6daf03e5d9628
Mrunal <80758264+msy1717@users.noreply.github.com>
2021-11-09 14:39:45 +0530

    Update __main__.py

--- beastx/__main__.py
+++ beastx/__main__.py
@@ -81,7 +81,7 @@ async def a():
 
     sed.info("Telegram String Session Wrong or Expired Please Add new one ") ; quit(1)
 import glob
-async def plud_channel():
+async def a():
             test1 = await bot.get_messages(plugin_channel, None , filter=InputMessagesFilterDocument) ; total = int(test1.total) ; total_doxx = range(0, total)
             for ixo in total_doxx:
                         mxo = test1[ixo].id ; await bot.download_media(await bot.get_messages(client, ids=mxo), "beastx/modules/")
