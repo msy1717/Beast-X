94714ea5759645232effd1ba84fa50fe85369d9e
Mrunal <80758264+msy1717@users.noreply.github.com>
2021-11-13 19:46:59 +0530

    Create BeastX_Userbot.py

---
+++ beastx/modules/BeastX_Userbot.py
@@ -0,0 +1,17 @@
+from . import *
+
+tgbotusername = Var.TG_BOT_USER_NAME_BF_HER
+
+from beastx import ALIVE_NAME, CMD_LIST, lang
+
+from beastx.utils import beastx_cmd
+
+from beastx import beast
+
+@beast_cmd(pattern=".repoo ?(.*)")
+
+async def repohand(event):
+
+    results = await beast.inline_query(tgbotusername,'repo')
+
+    await results[0].click(event.chat_id)
