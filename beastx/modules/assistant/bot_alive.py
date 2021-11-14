fb090ea6db37917c9347f9260990c9762ce99af7
Mrunal <80758264+msy1717@users.noreply.github.com>
2021-11-14 09:50:55 +0530

    Update bot_alive.py

--- beastx/modules/assistant/bot_alive.py
+++ beastx/modules/assistant/bot_alive.py
@@ -1,7 +1,7 @@
 from beastx import ALIVE_NAME
 from beastx.modules import currentversion
 
-DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "Unknown"
+DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "Beast UserPeru"
 PM_IMG = "https://telegra.ph/file/4a1e0ee716f805cf66777.jpg"
 pm_caption = "➥ **ASSISTANT IS:** `ONLINE`\n\n"
 pm_caption += "➥ **SYSTEMS STATS**\n"
@@ -11,10 +11,10 @@ pm_caption += "➥ **Database Status:**  `Functional`\n"
 pm_caption += "➥ **Current Branch** : `master`\n"
 pm_caption += f"➥ **Version** : `{currentversion}`\n"
 pm_caption += f"➥ **My Boss** : {DEFAULTUSER} \n\n"
-pm_caption += "➥ **Branch** :  [main](https://github.com/TeamEviral/Fire-X)\n"
+pm_caption += "➥ **Branch** :  [main](https://github.com/msy1717/Beast-X)\n"
 
 # only Owner Can Use it
 @assistant_cmd("alive", is_args=False)
 @peru_only
-async def fire(event):
+async def sed(event):
     await tgbot.send_file(event.chat_id, PM_IMG, caption=pm_caption)
