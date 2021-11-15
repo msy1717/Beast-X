6b12439e16397da34662c645346060160d46c166
Mrunal <80758264+msy1717@users.noreply.github.com>
2021-11-15 09:18:06 +0530

    Update carbon.py

--- beastx/modules/carbon.py
+++ beastx/modules/carbon.py
@@ -8,7 +8,7 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.keys import Keys
 
-from firebot import CHROME_DRIVER, CMD_HELP, GOOGLE_CHROME_BIN
+from beastx import CHROME_DRIVER, CMD_HELP, GOOGLE_CHROME_BIN
 from beastx.utils import beastx_cmd
 
 CARBONLANG = "auto"
