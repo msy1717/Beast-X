2a1c3c6799f9a261617c8a3fbaa0560bfad0c7c6
Mrunal <80758264+msy1717@users.noreply.github.com>
2021-11-17 16:50:50 +0530

    Update helper.py

--- beastx/modules/helper.py
+++ beastx/modules/helper.py
@@ -33,8 +33,7 @@ async def cmd_list(event):
             else:
                 await event.edit(input_str + " is not a valid plugin!")
         else:
-            help_string = f"""Beast-X plugins Are Listed Here !\n
-For More Help or Support contact {DEFAULTUSER}\nCurrently Used Language - {lang}"""
+            help_string = f"""Bot of {DEFAULTUSER}\n𝕄𝔸𝕀ℕ 𝕄𝔼ℕ𝕌\nTotal Commands:{len(CMD_LIST)}\n"""
             results = await bot.inline_query(  # pylint:disable=E0602
                 tgbotusername, help_string
             )
