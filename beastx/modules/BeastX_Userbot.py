e6a611233a831afabdf06a5ab34735f7ad7843df
Mrunal <80758264+msy1717@users.noreply.github.com>
2021-11-21 13:47:37 +0530

    Update BeastX_Userbot.py

--- beastx/modules/BeastX_Userbot.py
+++ beastx/modules/BeastX_Userbot.py
@@ -3,21 +3,22 @@ from . import *
 tgbotusername = Var.TG_BOT_USER_NAME_BF_HER
 
 from beastx import ALIVE_NAME, CMD_LIST, lang
+from beastx.events import register
 
 
 
 
-#@beast_cmd(pattern=".repo ?(.*)")
+@register(pattern=".repo ?(.*)")
 
-#async def repohand(event):
+async def repohand(event):
 
-   # results = await beast.inline_query(tgbotusername,'repo')
+    results = await beast.inline_query(tgbotusername,'repo')
 
-    #await results[0].click(event.chat_id)
-@beast_cmd(outgoing=True, pattern="^.repo$")
-async def source(e):
-    if not e.text[0].isalpha() and e.text[0] not in ("/", "#", "@", "!"):
-        await e.edit("Hello , User I Am Using [BeastXUserbot](https://github.com/msy1717/Beast-X) ! Worth A Try 😌")
+    await results[0].click(event.chat_id)
+#@beast_cmd(outgoing=True, pattern="^.repo$")
+#async def source(e):
+    #if not e.text[0].isalpha() and e.text[0] not in ("/", "#", "@", "!"):
+        #await e.edit("Hello , User I Am Using [BeastXUserbot](https://github.com/msy1717/Beast-X) ! Worth A Try 😌")
 			  
             
             
