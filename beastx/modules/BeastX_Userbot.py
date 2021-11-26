0c351dff00ce1b058106bfb1a65a761ff199fb80
Mrunal <80758264+msy1717@users.noreply.github.com>
2021-11-26 13:22:23 +0530

    Update BeastX_Userbot.py

--- beastx/modules/BeastX_Userbot.py
+++ beastx/modules/BeastX_Userbot.py
@@ -8,17 +8,17 @@ from beastx.events import register
 
 
 
-@register(pattern=".repo ?(.*)")
+#@register(pattern=".repo ?(.*)")
 
-async def repohand(event):
+#async def repohand(event):
 
-    results = await beast.inline_query(tgbotusername,'repo')
+   # results = await beast.inline_query(tgbotusername,'repo')
 
-    await results[0].click(event.chat_id)
-#@beast_cmd(outgoing=True, pattern="^.repo$")
-#async def source(e):
-    #if not e.text[0].isalpha() and e.text[0] not in ("/", "#", "@", "!"):
-        #await e.edit("Hello , User I Am Using [BeastXUserbot](https://github.com/msy1717/Beast-X) ! Worth A Try 😌")
+    #await results[0].click(event.chat_id)
+@beast_cmd(outgoing=True, pattern="^.repo$")
+async def source(e):
+    if not e.text[0].isalpha() and e.text[0] not in ("/", "#", "@", "!"):
+        await e.edit("Hello , User I Am Using [BeastXUserbot](https://github.com/msy1717/Beast-X) ! Worth A Try 😌")
 			  
             
             
